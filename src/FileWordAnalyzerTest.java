7008b648d50df5ef3669c497c9e21a7adbe00572
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-02-22 10:08:43 +0100

    refactor the FileWordAnalyzerTest with setup method

--- src/FileWordAnalyzerTest.java
+++ src/FileWordAnalyzerTest.java
@@ -1,6 +1,6 @@
 package src;
 
-import org.junit.Test;
+import org.junit.Before;
 
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -10,13 +10,18 @@ import static org.junit.Assert.assertEquals;
 
 public class FileWordAnalyzerTest {
 
+    private FilePartReader read = new FilePartReader();
+    private FileWordAnalyzer analyzer = new FileWordAnalyzer(read);
+
+
+    @Before
+    public void setup() {
+        read.setup("texts/text.txt", 1, 3);
+    }
 
 
     @org.junit.Test
     public void getWordsOrderedAlphabetically() throws FileNotFoundException {
-        FilePartReader read = new FilePartReader();
-        read.setup("texts/text.txt", 1, 3);
-        FileWordAnalyzer analyzer = new FileWordAnalyzer(read);
         List<String> result = analyzer.getWordsOrderedAlphabetically();
         List<String> expected = Arrays.asList("aha", "anna", "First", "line", "line", "line", "Second", "Third");
         assertEquals(expected, result);
@@ -24,9 +29,6 @@ public class FileWordAnalyzerTest {
 
     @org.junit.Test
     public void getWordsContainingSubstring() throws FileNotFoundException {
-        FilePartReader read = new FilePartReader();
-        read.setup("texts/text.txt", 1, 3);
-        FileWordAnalyzer analyzer = new FileWordAnalyzer(read);
         List<String> result = analyzer.getWordsContainingSubstring("ine");
         List<String> expected = Arrays.asList("line", "line", "line");
         assertEquals(expected, result);
@@ -34,16 +36,9 @@ public class FileWordAnalyzerTest {
 
     @org.junit.Test
     public void getStringsWhichPalindromes() throws FileNotFoundException {
-        FilePartReader read = new FilePartReader();
-        read.setup("texts/text.txt", 1, 3);
-        FileWordAnalyzer analyzer = new FileWordAnalyzer(read);
         List<String> result = analyzer.getStringsWhichPalindromes();
         List<String> expected = Arrays.asList("aha", "anna");
         assertEquals(expected, result);
     }
 
-    @Test
-    public void justFunny(){
-        assertEquals(11,11);
-    }
 }
