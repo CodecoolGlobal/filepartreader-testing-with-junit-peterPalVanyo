92557830c784aee02c2c35c6d7829b12ee74d7e7
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-02-23 06:53:18 +0100

    the filePartReaderTest to the zero (setup) method

--- src/FilePartReaderTest.java
+++ src/FilePartReaderTest.java
@@ -4,32 +4,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
-
 import static org.junit.Assert.assertEquals;
 
 public class FilePartReaderTest {
 
-    private static FilePartReader read;
-    private static String result;
+    private FilePartReader read;
+    private String result;
 
     @Before
     public void init() throws FileNotFoundException {
-        FilePartReader read = new FilePartReader();
+        read = new FilePartReader();
         read.setup("texts/text.txt", 1, 1);
         result = read.read();
     }
 
-//    @Rule
-//    public ExpectedException thrown= ExpectedException.none();
-//
-//    @Test
-//    public void setup() {
-//        thrown.expect(IllegalArgumentException.class);
-//        read.setup("texts/text.txt", 3, 1);
-//    }
+    @Test(expected = IllegalArgumentException.class)
+    public void setup() {
+        read.setup("texts/text.txt", 3, 1);
+    }
 
     @Test
-    public void read() throws FileNotFoundException {
+    public void read() {
         String expected = "First line anna\nSecond line\nThird line aha\n";
         assertEquals(expected, result);
     }
